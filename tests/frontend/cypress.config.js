8dd666963bcce2206bcfac6056df88662afdacc5
Diksha <97282430+dvig14@users.noreply.github.com>
2025-09-17 16:11:44 +0530

    Update cypress.config.js

--- tests/frontend/cypress.config.js
+++ tests/frontend/cypress.config.js
@@ -1,5 +1,6 @@
 const { defineConfig } = require('cypress')
 
+// BASE_URL get from env set in pipeline
 module.exports = defineConfig({
   e2e: {
     baseUrl: process.env.FRONTEND_BASE_URL || 'http://localhost:3000',
